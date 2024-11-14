b0bf56414ab31ba155c3fe4d07f6d5b2e30986c6
Michael Primeaux <mprimeaux@users.noreply.github.com>
2024-11-14 10:01:43 -0600

    FEATURE: Initial Commit (#1)

---
+++ Formula/nanoid.rb
@@ -0,0 +1,52 @@
+class NanoidCli < Formula
+  desc "Nano ID Command Line Interface"
+  homepage "https://github.com/sixafter/nanoid-cli"
+  license "Apache-2.0"
+
+  version "1.0.0"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/sixafter/nanoid-cli/releases/download/v#{version}/nanoid_#{version}_darwin_arm64.tar.gz"
+      sha256 "4e9f63ad3a67904f3c8b20503a3e075b8893060d08032ef75303f8db57bfa4a6"
+    end
+
+    on_intel do
+      url "https://github.com/sixafter/nanoid-cli/releases/download/v#{version}/nanoid_#{version}_darwin_amd64.tar.gz"
+      sha256 "82e89e5c482e013ece7b838dfe12b44cabec347ecd84c89c43ef3b4265d47b3a"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/sixafter/nanoid-cli/releases/download/v#{version}/nanoid_#{version}_linux_arm64.tar.gz"
+      sha256 "PLACEHOLDER_SHA256_LINUX_ARM648074c568972d7db02347a89698aab147e784527bb06f0a8e3c698625500bcde4_TAR"
+    end
+
+    on_intel do
+      url "https://github.com/sixafter/nanoid-cli/releases/download/v#{version}/nanoid_#{version}_linux_amd64.tar.gz"
+      sha256 "b24eca88e1c08adac76770d082ac6ddc32dddacb0a060fa6fb6b5c1798896506"
+    end
+  end
+
+  on_windows do
+    on_arm do
+      url "https://github.com/sixafter/nanoid-cli/releases/download/v#{version}/nanoid_#{version}_windows_arm64.zip"
+      sha256 "cb16ea09bcb13be547d4f5cde81732baa163f7dc08c0f08f96582ec61b4a2716"
+    end
+
+    on_intel do
+      url "https://github.com/sixafter/nanoid-cli/releases/download/v#{version}/nanoid_#{version}_windows_amd64.zip"
+      sha256 "9e0c2a0842bdd4441bd46487481d7c0e52d6ad75e14c9da907f4b124163dc693"
+    end
+  end
+
+  def install
+    bin.install "nanoid"
+  end
+
+  test do
+    # Basic version check
+    assert_match "version", shell_output("#{bin}/nanoid --version")
+  end
+end
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# homebrew-nanoid-cli
+# homebrew-tap [![Powered By: GoReleaser](https://img.shields.io/badge/powered%20by-goreleaser-green.svg?style=flat-square)](https://github.com/goreleaser)
+
+## Formulas
+
+The following formulas are available in this tap:
+
+- [**`Nano ID CLI`**](Formula/nanoid.rb) - A simple, fast, and concurrent command-line tool for generating secure, URL-friendly unique string IDs using the [NanoID](https://github.com/sixafter/nanoid-cli) implementation.
+
+## Usage
+
+```shell
+brew tap sixafter/tap
+brew install nanoid
+```
